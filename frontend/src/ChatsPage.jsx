9283613ac948f19653f6dc17485fc10e091d63c7
houssem <houssemfraj5@gmail.com>
2023-03-04 00:23:15 +0000

    done

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,20 @@
-const ChatsPage =()=>{
-    return<>chats...</>
+import {MultiChatSocket,MultiChatWindow,useMultiChatLogic} from "react-chat-engine-advanced"
+
+
+
+const ChatsPage =(props)=>{
+    const chatProps =useMultiChatLogic(
+        "a74f889f-20d6-4713-8415-184844923858",
+        props.user.username,
+        props.user.secret)
+    return(
+        <div style={{height: '100vh'}}>
+<MultiChatSocket {...chatProps} />
+<MultiChatWindow {...chatProps} style={{height: '100%'}} />
+
+
+        </div>
+    )
 }
 
 
